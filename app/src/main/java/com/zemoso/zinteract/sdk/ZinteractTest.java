43b20133cdf60b2ce88a48f01875288a24146dd6
Praveen Yadav <praveen@Snappy.local>
2015-01-28 18:04:56 +0530

    Added Test Cases

--- app/src/main/java/com/zemoso/zinteract/sdk/ZinteractTest.java
+++ app/src/main/java/com/zemoso/zinteract/sdk/ZinteractTest.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.util.Pair;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -21,17 +22,20 @@ public class ZinteractTest extends ApplicationTestCase<Application> {
 
     private static final String TAG = "com.zemoso.zinteract.sdk.ZinteractTest";
 
+    private DbHelper dbHelper;
+
     public void setUp(){
         createApplication();
         getContext().getSharedPreferences(Constants.Z_SHARED_PREFERENCE_USER_PROPERTIES_FILE_NAME,Context.MODE_PRIVATE).edit().clear().commit();
         getContext().getSharedPreferences(Constants.Z_SHARED_PREFERENCE_DATASTORE_FILE_NAME,Context.MODE_PRIVATE).edit().clear().commit();
 
         getContext().getSharedPreferences(Constants.Z_SHARED_PREFERENCE_FILE_NAME,Context.MODE_PRIVATE).edit().clear().commit();
-
+        dbHelper = DbHelper.getDatabaseHelper(getContext());
         Zinteract.initializeWithContextAndKey(getContext(),"TestAndroidAPIKey");
     }
 
     public void tearDown(){
+        getContext().deleteDatabase(Constants.Z_DB_NAME);
         terminateApplication();
     }
 
@@ -48,7 +52,7 @@ public class ZinteractTest extends ApplicationTestCase<Application> {
     public void testUserProperty(){
         String value = "John";
         String key = "firstname";
-        assertEquals("default",Zinteract.getUserProperty(key,"default"));
+        assertEquals("default", Zinteract.getUserProperty(key, "default"));
         Zinteract.setUserProperty("firstname", value);
         assertEquals(value,Zinteract.getUserProperty(key,"default"));
     }
@@ -56,11 +60,19 @@ public class ZinteractTest extends ApplicationTestCase<Application> {
     public void testLogEvent(){
         String eventName = "click";
         Zinteract.logEvent(eventName);
-        DbHelper dbHelper = DbHelper.getDatabaseHelper(getContext());
+
         try {
-            Pair<Long,JSONArray> p = dbHelper.getEvents(10,-1);
-            assertEquals(1,p.second.length());
-            //assertEquals(eventName,p.second.getJSONObject(0).getString("eventName"));
+            Thread.sleep(1000);
+            Pair<Long,JSONArray> p = dbHelper.getEvents(-1,-1);
+            boolean clickEventFound = false;
+            for(int i=0; i < p.second.length();i++){
+                JSONObject json = p.second.getJSONObject(i);
+                if(json.getString("eventName").equals(eventName)){
+                    clickEventFound = true;
+                    break;
+                }
+            }
+            assertEquals(true,clickEventFound);
         }
         catch (Exception e){
             Log.e(TAG, "Exception :" + e);
