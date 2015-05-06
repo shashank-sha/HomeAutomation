5f534c37aabad60e5c096a5df4fdd70e16f0b35f
Vedaprakash <vedapraksdh@zemosolabs.com>
2015-05-06 17:54:59 +0530

    Activity4 modified to include purchase features

--- app/src/main/java/com/zemoso/zinteract/ZinteractSampleApp/Activity4.java
+++ app/src/main/java/com/zemoso/zinteract/ZinteractSampleApp/Activity4.java
@@ -2,21 +2,42 @@ package com.zemoso.zinteract.ZinteractSampleApp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.zemosolabs.zinteract.sdk.Zinteract;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class Activity4 extends Activity {
+import java.text.DecimalFormat;
+
+
+public class Activity4 extends Activity implements View.OnClickListener {
+    private Float grandTotal=0.00f;
+    private DecimalFormat df = new DecimalFormat("#.##");
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity4);
+        Button addToCart = (Button)findViewById(R.id.buttonAddToCart);
+        addToCart.setOnClickListener(this);
+        Button buy = (Button)findViewById(R.id.buttonBuy);
+        buy.setOnClickListener(this);
+        Button removeFromCart = (Button)findViewById(R.id.buttonRemove);
+        removeFromCart.setOnClickListener(this);
+        prefs = getSharedPreferences("PurchasePrefs",Activity.MODE_PRIVATE);
+        grandTotal = prefs.getFloat("grandTotal",0.00f);
+        ((TextView)findViewById(R.id.grandTotalValue)).setText(df.format(grandTotal));
     }
 
 
@@ -46,10 +67,12 @@ public class Activity4 extends Activity {
     public void onResume(){
         super.onResume();
         Zinteract.logEvent("view screen4");
+        getSharedPreferences("PurchasePrefs",Activity.MODE_PRIVATE).getFloat("grandTotal",0.00f);
     }
 
     @Override
     public void onPause(){
+        getSharedPreferences("PurchasePrefs",Activity.MODE_PRIVATE).edit().putFloat("grandTotal",grandTotal).apply();
         super.onPause();
     }
 
@@ -65,5 +88,34 @@ public class Activity4 extends Activity {
         Zinteract.logEvent("clicked to view screen3");
         Intent intent = new Intent(Activity4.this, Activity3.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.buttonBuy:
+                String grandTotalString = (String)((TextView)findViewById(R.id.grandTotalValue)).getText();
+                JSONObject purchaseDetails = new JSONObject();
+                try {
+                    purchaseDetails.put("grand_total",Double.valueOf(grandTotalString));
+                } catch (JSONException e) {
+                    Log.e("Purchase","Activity 4 update of grand total failed",e);
+                }
+                Zinteract.logPurchaseCompletedEvent(purchaseDetails);
+                grandTotal = 0.00f;
+                ((TextView)findViewById(R.id.grandTotalValue)).setText(df.format(grandTotal));
+                break;
+            case R.id.buttonAddToCart:
+                grandTotal += (float)(Math.random()*5000);
+                ((TextView)findViewById(R.id.grandTotalValue)).setText(df.format(grandTotal));
+                Zinteract.logPurchaseAttempted();
+                break;
+            case R.id.buttonRemove:
+                grandTotal = 0.00f;
+                ((TextView)findViewById(R.id.grandTotalValue)).setText(df.format(grandTotal));
+                break;
+            default:
+                break;
+        }
     }
 }
