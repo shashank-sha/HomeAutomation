875e7dbb62e0f182d4117a9dc4f77342c9683c50
Vedaprakash <vedapraksdh@zemosolabs.com>
2015-09-07 14:37:54 +0530

    Demo build for Akash tutorials ~

--- app/src/main/java/com/zemoso/zetarget/ZeTargetSampleApp/MainActivity.java
+++ app/src/main/java/com/zemoso/zetarget/ZeTargetSampleApp/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
+        //Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         /*Initialize ZeTarget with Context and key provided upon registration on ZeTarget website.
